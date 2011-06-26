3c0f0495094fcb1049a39a687c04f20ffc99dcd9
Ando Roots <ando@roots.ee>
2011-06-26 15:09:10 +0300

    Install/uninstall hooks

---
+++ uninstall.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Included during plugin uninstall.
+ * Removes database tables
+ */
+if (defined(WP_UNINSTALL_PLUGIN)) {
+    require_once (ABSPATH.'config.php');
+    require_once ('includes/models/main.php');
+    global $wpdb;
+
+    $theatreTroupe = New Theatre_Troupe();
+
+    // Delete TheatreTroupe database tables
+    $sql = "DROP TABLE IF EXISTS $wpdb->ttroupe_series;";
+    dbDelta($sql);
+    $sql = "DROP TABLE IF EXISTS $wpdb->ttroupe_shows;";
+    dbDelta($sql);
+    $sql = "DROP TABLE IF EXISTS $wpdb->ttroupe_show_participants;";
+    dbDelta($sql);
+
+    $table = $wpdb->prefix.'usermeta';
+    $sql = "DELETE FROM $table WHERE meta_key LIKE '%ttroupe_%';";
+    dbDelta($sql);
+}
+
+?>
